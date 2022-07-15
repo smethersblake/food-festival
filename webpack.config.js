19b40b7680153cb84582417a68682270c2ec967a
Blake Smethers <96027095+smethersblake@users.noreply.github.com>
2022-07-15 16:27:40 -0500

    add script bundles

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,6 @@
-// const path = require('path')
-// const webpack = require("webpack")
-// require("bootstrap");
-
-
-
-// module.exports = {
-//     entry: './assets/js/script.js',
-//     output: {
-//         path: path.resolve(__dirname, 'dist'),
-//         filename: 'main.bundle.js'
-//     },
-//     plugins:[
-//     new webpack.ProvidePlugin({
-//         $: "jquery",
-//         jQuery: "jquery"
-//     }),
-// ],
-//     mode: 'development'
-// }
-
 const webpack = require("webpack")
 const path = require("path");
+const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   entry: "./assets/js/script.js",
@@ -33,6 +13,9 @@ module.exports = {
     $: "jquery",
     jQuery: "jquery"
   }),
+    new BundleAnalyzerPlugin({
+      analyzerMode: 'static', // the report outputs to an HTML file in the dist folder
+    })
   ],
   mode: "development"
 };
